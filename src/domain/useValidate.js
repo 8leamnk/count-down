36226680587a099bd07fb1d7d1a661159c1a3b9e
8leamnk <silkviewrk@gmail.com>
2023-12-26 14:07:28 +0900

    fix: 유효성 검사 기능을 수정
    수정 내용:
    - 기존의 불필요한 상태 관리를 삭제
    - 상태값 일괄처리로 인해 반영이 잘 되지 않는 부분을 Map을 사용하여 처리하도록 로직을 변경

--- src/domain/useValidate.js
+++ src/domain/useValidate.js
@@ -1,29 +1,16 @@
-import { useCallback, useState } from 'react';
+import { useCallback } from 'react';
 import MESSAGE from '../constants/message';
+import VALUE from '../constants/value';
 
-const NOT_NUMBER = /[^0-9]/;
 const RANGE = Object.freeze({
   minutes: 0,
   second: 1,
   min: 0,
-  max: 3599,
+  max: 3599 * VALUE.msUnit,
 });
-const TIME_UNIT = 60;
 const ERROR_RETURN = 0;
 
 function useValidate() {
-  const [answerTime, setAnswerTime] = useState({
-    minutes: 0,
-    second: 0,
-  });
-
-  const validateNumber = useCallback((answer) => {
-    if (NOT_NUMBER.test(answer)) {
-      window.alert(MESSAGE.error.notNumber);
-      return ERROR_RETURN;
-    }
-  }, []);
-
   const validateRange = useCallback((answer, type) => {
     const number = Number(answer);
     const range = RANGE[type];
@@ -31,44 +18,45 @@ function useValidate() {
     if (!Number.isSafeInteger(range) || number < range) {
       window.alert(MESSAGE.error.rangeEach);
       return ERROR_RETURN;
-    } else {
-      setAnswerTime((curState) => ({ ...curState, [type]: number }));
     }
+
+    return number;
   }, []);
 
-  const calculateinitialTime = useCallback(() => {
-    const { minutes, second } = answerTime;
-    const initialTime = minutes * TIME_UNIT + second;
+  const calculateinitialTime = useCallback((inputsMap) => {
+    const [minutes, second] = [...inputsMap.values()];
+    const initialTime = minutes * VALUE.timeUnit + second;
 
-    return initialTime;
-  }, [answerTime]);
+    return initialTime * VALUE.msUnit;
+  }, []);
 
-  const validateTotalTime = useCallback(() => {
-    const { min, max } = RANGE;
-    const initialTime = calculateinitialTime();
+  const validateTotalTime = useCallback(
+    (inputsMap) => {
+      const { min, max } = RANGE;
+      const initialTime = calculateinitialTime(inputsMap);
 
-    if (initialTime < min || initialTime > max) {
-      window.alert(MESSAGE.error.rangeTotal);
-      return ERROR_RETURN;
-    }
+      if (initialTime < min || initialTime > max) {
+        window.alert(MESSAGE.error.rangeTotal);
+        return ERROR_RETURN;
+      }
 
-    return initialTime;
-  }, [calculateinitialTime]);
+      return initialTime;
+    },
+    [calculateinitialTime],
+  );
 
   const validate = useCallback(
     (inputs) => {
-      const inputsArr = Object.entries(inputs);
+      const inputsMap = new Map(Object.entries(inputs));
 
-      for (let i = 0; i < inputsArr.length; i += 1) {
-        const [type, answer] = inputsArr[i];
+      inputsMap.forEach((answer, type) => {
+        const number = validateRange(answer, type);
+        inputsMap.set(type, number);
+      });
 
-        validateNumber(answer);
-        validateRange(answer, type);
-      }
-
-      return validateTotalTime();
+      return validateTotalTime(inputsMap);
     },
-    [validateNumber, validateRange, validateTotalTime],
+    [validateRange, validateTotalTime],
   );
 
   return { validate };
