b86cee5f12c7e98cf63f2a86aeab71875a5b3414
8leamnk <silkviewrk@gmail.com>
2023-12-26 14:06:24 +0900

    test: 타이머 관리 기능 테스트

--- src/domain/__tests__/useTimeTest.js
+++ src/domain/__tests__/useTimeTest.js
@@ -9,29 +9,29 @@ describe('타이머 관리 테스트', () => {
     const OUTPUT = 3000;
 
     // when
-    const { result } = renderHook((props) => useTime(props), {
-      initialProps: INITIAL_TIME,
+    const { result } = renderHook(() => useTime());
+    act(() => {
+      result.current.handleTime(INITIAL_TIME);
     });
-    const { time } = result.current;
 
     // then
-    expect(time).toBe(OUTPUT);
+    expect(result.current.time).toBe(OUTPUT);
   });
 
   test('카운트 다운이 시작되면 3초 후에 3초만큼 time 상태가 감소한다.', () => {
     // given
     const INITIAL_TIME = 5000;
-    const MS = 1000;
     const AFTER_TIME = 3000;
     const OUTPUT = 2000;
 
     jest.useFakeTimers();
 
     // when
-    const { result } = renderHook((props) => useTime(props), {
-      initialProps: INITIAL_TIME,
+    const { result } = renderHook(() => useTime());
+    act(() => {
+      result.current.handleTime(INITIAL_TIME);
+      result.current.createTimeId();
     });
-    result.current.createTimeId(MS);
 
     // then
     expect(result.current.time).toBe(INITIAL_TIME);
