bdf8efc97c3a119c2dcf7f8dcb3cf92e85a8572b
8leamnk <silkviewrk@gmail.com>
2024-01-02 18:39:01 +0900

    test: 유효성 검사 기능 테스트

--- src/domain/__tests__/useValidateTest.js
+++ src/domain/__tests__/useValidateTest.js
@@ -1,7 +1,16 @@
 import { renderHook } from '@testing-library/react';
 import useValidate from '../useValidate';
 
-window.alert = jest.fn();
+const handlePopup = jest.fn();
+const ERROR_NUMBER = Object.freeze({
+  title: 'ERROR',
+  description: '0 이상의 숫자를 입력해 주세요.',
+});
+const ERROR_RANGE = Object.freeze({
+  title: 'ERROR',
+  description: '최소 0분 1초, 최대 59분 59초의 시간을 입력해 주세요.',
+});
+const ERROR_RETURN = 0;
 
 describe('입력값 유효성 검사 기능 테스트', () => {
   test.each([
@@ -9,11 +18,14 @@ describe('입력값 유효성 검사 기능 테스트', () => {
     [{ minutes: '0', second: '-1' }],
   ])('각각 0 이상의 숫자를 입력하지 않으면 예외가 발생한다.', (inputs) => {
     // when
-    const { result } = renderHook(() => useValidate());
-    result.current.getInitialTime(inputs);
+    const { result } = renderHook((props) => useValidate(props), {
+      initialProps: { inputs, handlePopup },
+    });
+    const initialTime = result.current.getInitialTime();
 
     // then
-    expect(window.alert).toHaveBeenCalledWith('0 이상의 숫자를 입력해 주세요.');
+    expect(handlePopup).toHaveBeenCalledWith(ERROR_NUMBER);
+    expect(initialTime).toBe(ERROR_RETURN);
   });
 
   test('총 1초 이상의 숫자를 입력하지 않으면 예외가 발생한다.', () => {
@@ -21,13 +33,14 @@ describe('입력값 유효성 검사 기능 테스트', () => {
     const INPUTS = { minutes: '0', second: '0' };
 
     // when
-    const { result } = renderHook(() => useValidate());
-    result.current.getInitialTime(INPUTS);
+    const { result } = renderHook((props) => useValidate(props), {
+      initialProps: { inputs: INPUTS, handlePopup },
+    });
+    const initialTime = result.current.getInitialTime();
 
     // then
-    expect(window.alert).toHaveBeenCalledWith(
-      '최소 0분 1초, 최대 59분 59초의 시간을 입력해 주세요.',
-    );
+    expect(handlePopup).toHaveBeenCalledWith(ERROR_RANGE);
+    expect(initialTime).toBe(ERROR_RETURN);
   });
 
   test('총 3600초 이상의 숫자를 입력하면 예외가 발생한다.', () => {
@@ -35,12 +48,13 @@ describe('입력값 유효성 검사 기능 테스트', () => {
     const INPUTS = { minutes: '60', second: '0' };
 
     // when
-    const { result } = renderHook(() => useValidate());
-    result.current.getInitialTime(INPUTS);
+    const { result } = renderHook((props) => useValidate(props), {
+      initialProps: { inputs: INPUTS, handlePopup },
+    });
+    const initialTime = result.current.getInitialTime();
 
     // then
-    expect(window.alert).toHaveBeenCalledWith(
-      '최소 0분 1초, 최대 59분 59초의 시간을 입력해 주세요.',
-    );
+    expect(handlePopup).toHaveBeenCalledWith(ERROR_RANGE);
+    expect(initialTime).toBe(ERROR_RETURN);
   });
 });
