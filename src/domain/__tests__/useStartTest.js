fee52e0fc30befdb442825c51852c2235264494e
8leamnk <silkviewrk@gmail.com>
2024-01-27 19:03:02 +0900

    refactor: 시작 기능 리팩토링
    변경 내용:
    - 복잡한 의존성을 줄임
    - 유효성 검사가 어디서 이루어지는지 쉽게 파악이 되지 않아 이를 컨트롤러에서 처리하는 것으로 변경
    - 변경 내용을 테스트

--- src/domain/__tests__/useStartTest.js
+++ src/domain/__tests__/useStartTest.js
@@ -1,23 +1,21 @@
 import { act, renderHook } from '@testing-library/react';
 import useStart from '../useStart';
 
-const getInitialTime = jest.fn();
 const handleTime = jest.fn();
 const createTimeId = jest.fn();
 
 describe('시작 기능 테스트', () => {
   test('시간 값이 0이면 카운트 다운을 시작하지 않는다.', () => {
     // given
+    const INITIAL_TIME = 0;
     const IS_START = false;
     const CALLED_TIMES = 0;
-
-    getInitialTime.mockImplementationOnce(() => 0);
 
     // when
     const { result } = renderHook((props) => useStart(props));
 
     act(() => {
-      result.current.handleStart(getInitialTime, handleTime, createTimeId);
+      result.current.handleStart(INITIAL_TIME, handleTime, createTimeId);
     });
 
     // then
@@ -31,13 +29,11 @@ describe('시작 기능 테스트', () => {
     const IS_START = true;
     const CALLED_TIMES = 1;
 
-    getInitialTime.mockImplementationOnce(() => INITIAL_TIME);
-
     // when
     const { result } = renderHook((props) => useStart(props));
 
     act(() => {
-      result.current.handleStart(getInitialTime, handleTime, createTimeId);
+      result.current.handleStart(INITIAL_TIME, handleTime, createTimeId);
     });
 
     // then
