c8da9b31f0e3d99454cddcbfb9d25e1a95e4637f
8leamnk <silkviewrk@gmail.com>
2024-02-05 18:59:50 +0900

    refactor: 유효성 검사 기능 수정
    변경 내용:
    - 지수(exponent)로 initialTime을 계산하기 위해 inputs 데이터를 삽입된 순서대로 반복되는 Map 객체로 변경하여 이를 가지고 사용하도록 변경

--- src/domain/useValidation.js
+++ src/domain/useValidation.js
@@ -3,16 +3,22 @@ import { useSetRecoilState } from 'recoil';
 import { popupInfoState } from '../states/popup.states';
 import VALUE from '../constants/value';
 
+const TIME_MAP = new Map([
+  ['hour', 0],
+  ['minute', 0],
+  ['second', 0],
+]);
 const ERROR_MESSAGE = Object.freeze({
   notNumber: '숫자만 입력해 주세요.',
   rangeEach: '0 이상의 숫자를 입력해 주세요.',
   rangeTotal: '최소 0분 1초, 최대 59분 59초의 시간을 입력해 주세요.',
 });
 const RANGE = Object.freeze({
-  minutes: 0,
+  hour: 0,
+  minute: 0,
   second: 0,
   min: 1,
-  max: 3599 * VALUE.msUnit,
+  max: 3599,
 });
 const ERROR_INFO = Object.freeze({
   return: 0,
@@ -22,55 +28,65 @@ const ERROR_INFO = Object.freeze({
 function useValidation() {
   const setPopupInfo = useSetRecoilState(popupInfoState);
 
-  const validateRange = useCallback((type, answer) => {
-    const number = Number(answer);
-    const range = RANGE[type];
+  const convertToMap = useCallback((inputs) => {
+    const inputMap = new Map([...TIME_MAP]);
+
+    Object.entries(inputs).forEach(([type, answer]) => {
+      const number = Number(answer);
+
+      if (number) {
+        inputMap.set(type, number);
+      }
+    });
+
+    return inputMap;
+  }, []);
+
+  const validateRange = useCallback((key, number) => {
+    const range = RANGE[key];
 
     if (!Number.isSafeInteger(range) || number < range) {
       throw new Error(ERROR_MESSAGE.rangeEach);
     }
-
-    return number;
   }, []);
 
-  const calculateInitialTime = useCallback((numbers) => {
-    const initialTime = numbers.reduce((acc, cur, index) => {
-      const exponent = numbers.length - index - 1;
-      const number = cur * VALUE.timeUnit ** exponent;
+  const calculateInitialTime = useCallback((inputMap) => {
+    let initialTime = 0;
+    let exponent = inputMap.size - 1;
 
-      return acc + number;
-    }, 0);
+    inputMap.forEach((value) => {
+      initialTime += value * VALUE.timeUnit ** exponent;
+      exponent -= 1;
+    });
 
-    return initialTime * VALUE.msUnit;
+    return initialTime;
   }, []);
 
   const validateInitialTime = useCallback(
-    (numbers) => {
+    (inputMap) => {
       const { min, max } = RANGE;
-      const initialTime = calculateInitialTime(numbers);
+      const initialTime = calculateInitialTime(inputMap);
 
       if (initialTime < min || initialTime > max) {
         throw new Error(ERROR_MESSAGE.rangeTotal);
       }
 
-      return initialTime;
+      return initialTime * VALUE.msUnit;
     },
     [calculateInitialTime],
   );
 
   const validate = useCallback(
     (inputs) => {
-      const numbers = [];
+      const inputMap = convertToMap(inputs);
 
-      Object.entries(inputs).forEach(([type, answer]) => {
-        const number = validateRange(type, answer);
-
-        numbers.push(number);
+      inputMap.forEach((number, key) => {
+        validateRange(key, number);
       });
 
-      return validateInitialTime(numbers);
+      return validateInitialTime(inputMap);
     },
-    [validateRange, validateInitialTime],
+    [convertToMap, validateRange, validateInitialTime],
   );
 
   const getInitialTime = useCallback(
