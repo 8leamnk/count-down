fb3c58561a59d0a1e0a1490b4e77d214c160da66
8leamnk <silkviewrk@gmail.com>
2024-03-05 14:29:56 +0900

    refactor: 입력 기능 리팩토링
    변경 내용:
    - Atom 단위 추가
    - 컴포넌트명 변경

---
+++ src/Components/Atoms/Label.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import styled from 'styled-components';
+
+// style
+const S = {};
+
+S.Label = styled.label`
+  color: ${({ theme }) => theme.colors.primary};
+  font-size: ${({ theme }) => `${theme.fontSize.base}px`};
+  font-weight: 400;
+  letter-spacing: -0.4px;
+`;
+
+S.LabelText = styled.span`
+  display: inline-block;
+  margin-bottom: 4px;
+`;
+
+function Label({ labelText, children, ...rest }) {
+  return (
+    <S.Label {...rest}>
+      <S.LabelText>{labelText}</S.LabelText>
+      {children}
+    </S.Label>
+  );
+}
+
+export default React.memo(Label);
