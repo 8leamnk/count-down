f705b785cd390309e3d5e0b2656219f4d0e74e99
8leamnk <silkviewrk@gmail.com>
2024-03-05 15:04:07 +0900

    feat: 뒤로가기 기능 추가
    변경 내용:
    - 홈으로 가기에서 뒤로 가기로 변경
    - Atom 단위의 컴포넌트 추가

--- src/Components/Pages/NotFound.js
+++ src/Components/Pages/NotFound.js
@@ -1,43 +1,32 @@
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 import MainLayout from '../Layout/MainLayout';
+import Title from '../Atoms/Title';
 import Button from '../Atoms/Button';
 
 // style
 const S = {};
 
-S.Title = styled.h1`
-  color: ${({ theme }) => theme.colors.primary};
-  font-size: ${({ theme }) => `${theme.fontSize.xLarge}px`};
-  font-weight: 700;
-  letter-spacing: -0.8px;
+S.Title = styled(Title)`
   text-align: center;
   margin-bottom: 20px;
 `;
 
 S.BackBtn = styled(Button)`
   width: 100%;
+  font-size: ${({ theme }) => `${theme.fontSize.large}px`};
+  font-weight: 900;
   background-color: ${({ theme }) => theme.colors.red001};
-
-  a {
-    width: 100%;
-    height: 100%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    color: ${({ theme }) => theme.colors.white};
-    font-size: ${({ theme }) => `${theme.fontSize.large}px`};
-    font-weight: 900;
-  }
+  cursor: pointer;
 `;
 
 function NotFound() {
+  const navigate = useNavigate();
+
   return (
     <MainLayout>
       <S.Title>404 Not Found</S.Title>
-      <S.BackBtn>
-        <Link to="/">Go to home</Link>
-      </S.BackBtn>
+      <S.BackBtn onClick={() => navigate(-1)}>Go back</S.BackBtn>
     </MainLayout>
   );
 }
